161447472deb34f4606a2a73b945212c864b6e9c
Rehfeldinio <Lukas@rehfeldt.me>
2026-02-11 18:35:10 +0100

    /motd-reload created

--- src/main/java/de/rehfeldinio/motd/MOTDChange.java
+++ src/main/java/de/rehfeldinio/motd/MOTDChange.java
@@ -1,14 +1,14 @@
 package de.rehfeldinio.motd;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import java.util.ArrayList;
-import java.util.Random;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MOTDChange extends JavaPlugin implements Listener {
 
@@ -20,6 +20,8 @@ public class MOTDChange extends JavaPlugin implements Listener {
         if ((this.getConfig().getBoolean("Motd.Enabled")) && (!this.getConfig().getStringList("Motd.Texts").isEmpty())) {
             this.getServer().getPluginManager().registerEvents(this, this);
         }
+        getCommand("motd-reload").setExecutor(new ReloadCommand(this));
+        getLogger().info("MOTDChange has been enabled!");
     }
 
     @Override
